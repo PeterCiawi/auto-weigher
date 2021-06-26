264f5e0b3c141954cddd8b87b663241caa484610
unknown <peterciawi06@gmail.com>
2021-06-26 15:00:57 +0700

    First commit

---
+++ AutoWeigher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace AutoWeigher
+{
+    public class AutoWeigher
+    {
+            SerialPort port;
+        public AutoWeigher(string portName)
+        {
+            port = new SerialPort(portName);
+            port.Open();
+        }
+        void Weight(double weight)
+        {
+            port.Write($"<{weight}>");
+        }
+
+        public event EventHandler<WeightDoneArgs> WeightDone;
+        
+        private void ReadLineFunction()
+        {
+            
+            while (true)
+            {
+                string n = Console.ReadLine();
+                n.Remove('<');
+                n.Remove('>');
+                double weight = Convert.ToDouble(n);
+                WeightDoneArgs args = new WeightDoneArgs();
+                args.Weight = weight;
+                WeightDone?.Invoke(this, args);
+            }
+        }
+        public void Begin()
+        {
+            Thread thread = new Thread(new ThreadStart(ReadLineFunction));
+            thread.Start();
+        } 
+    }
+}
